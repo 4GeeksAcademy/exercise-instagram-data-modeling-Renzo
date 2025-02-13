ad95c4067b476a9d61202f11730e210a86dc1535
relo-ga <40379438+relo-ga@users.noreply.github.com>
2025-02-13 04:09:10 +0000

    Instagram data modeling v1

--- src/models.py
+++ src/models.py
@@ -1,29 +1,49 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from sqlalchemy import create_engine, ForeignKey
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(nullable=False)
+    firstname: Mapped[str] = mapped_column(nullable=False)
+    lastname: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable = False, unique = True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+class Post(Base):
+    __tablename__ = 'post'
 
-    def to_dict(self):
-        return {}
+    id: Mapped[int] = mapped_column(primary_key = True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(backref='post')
+
+class Media(Base):
+    __tablename__ = 'media'
+    id: Mapped[int] = mapped_column(primary_key = True)
+    type: Mapped[str] = mapped_column(nullable = False)
+    url: Mapped[str] = mapped_column(nullable = False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key = True)
+    comment_text: Mapped[str] = mapped_column(nullable = False)
+    autor_id: Mapped[str] = mapped_column(ForeignKey("user.id"))
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id: Mapped[int] = mapped_column(primary_key = True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("username.id"))
+
+
 
 ## Draw from SQLAlchemy base
 try:
